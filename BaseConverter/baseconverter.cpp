10a53f1d13130411006173fd15365dd8347fe61c
RaulToto <djbrexman@gmail.com>
2017-04-20 18:02:43 -0500

    update

--- BaseConverter/baseconverter.cpp
+++ BaseConverter/baseconverter.cpp
@@ -7,39 +7,32 @@
   * @brief
   */
 #include "baseconverter.h"
-
-BaseConverter::BaseConverter()
-{
-
-}
-
 int BaseConverter::convertNtoDecimal(std::string number, int base)
 {
 
     //std::string str=intToString(number);
-    int result=0;
+
+    int result=0;//variable para guardar el resultado
     for (int i = number.length()-1; i>=0; i--) {
-        int intNumber=number[i];
-        if(number[i]>='A' && number[i] <='F')
+        int intNumber=number[i];//almacena el valor del caracter en la posicion i
+        if(number[i]>='A' && number[i] <='F')//si el caracter ASI es de A--F (esto para los hexadecimales)
         {
-            intNumber-=55;
+            intNumber-=55;//convierte a entero el valor 55 al valor del caracter en codgo ASI
         }
         else if(number[i]>='0' && number[i] <='9')
         {
             intNumber-=48;
         }
-        std::cout << number[i] <<  "---" << intNumber << std::endl;
-        result+=intNumber*(pow(base,i));
+        result+=intNumber*(pow(base,i));//almacena el resultado este por el metodo de polinomio numero*(base)^i
     }
-    std::cout <<  "The result is: "<<result <<std::endl;
+    std::cout <<  "The result is: "<<result <<std::endl;//imprime el resultado
 }
 int BaseConverter::convertDecimalToN(int number, int base)
 {
-    std::string list{};
     for (int i = 0; number!=0; ++i) {
-       int convert=number%base;
-       number /= base;
-       if(convert>=10 && convert<=15){
+       int convert=number%base;//calcula el reciduo 
+       number /= base;//divide el nuemro por la base y actualiza este 
+       if(convert>=10 && convert<=15){//en el caso de que el residuo sea >=10 <=15 le suma 55 al caracter
            convert+=55;
            char str=convert;
            std::cout << "The result is:" << str ;
@@ -52,16 +45,16 @@ int BaseConverter::convertDecimalToN(int number, int base)
 
 bool BaseConverter::checkDecimal(int number)
 {
-    int tem=number%10;
+    int tem=number%10; //modulo 10 de un numero simpre sera el ultimo numero
     std::string str=intToString(number);
     std::string str1= intToString(tem);
-    if(str[str.length()-1]==str1[0])
+    if(str[str.length()-1]==str1[0])//si el modulo del ultimo numero concuerda con el ultimo numero
         return true;
     else
         return false;
 }
 
-std::__cxx11::string BaseConverter::intToString(int number)
+std::__cxx11::string BaseConverter::intToString(int number)//convierte un entero en string
 {
     std::stringstream ss;
     ss << number;
@@ -71,7 +64,3 @@ std::__cxx11::string BaseConverter::intToString(int number)
 
 }
 
-//bool BaseConverter::acheckN(int numer)
-//{
-
-//}
